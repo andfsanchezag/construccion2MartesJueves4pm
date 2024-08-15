52e70f9d941229d6330162cee31319bb081afd76
andfsanchezag <andres.sanchez57@tdea.edu.co>
2024-08-15 16:22:15 -0500

    commit para ejemplo

--- proyectoEjemplo/src/main/java/app/App.java
+++ proyectoEjemplo/src/main/java/app/App.java
@@ -5,7 +5,7 @@ import app.controller.ControllerInterface;
 import app.controller.LoginController;
 
 public class App {
-
+//comentario para commit
 	public static void main(String[] args) throws Exception {
 		ControllerInterface controller = new LoginController();
 		try {
