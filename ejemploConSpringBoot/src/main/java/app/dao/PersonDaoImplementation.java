051a17c41bd9f384d8b57bc36e5d1226e5e57103
andfsanchezag <andres.sanchez57@tdea.edu.co>
2024-09-12 16:42:19 -0500

    add

---
+++ ejemploConSpringBoot/src/main/java/app/dao/PersonDaoImplementation.java
@@ -0,0 +1,38 @@
+package app.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import app.config.MYSQLConnection;
+import app.dao.interfaces.PersonDao;
+import app.dao.repository.PersonRepository;
+import app.dto.PersonDto;
+import app.helpers.Helper;
+import app.model.Person;
+
+public class PersonDaoImplementation implements PersonDao {
+        public PersonRepository personRepository;
+	@Override
+	public boolean existsByDocument(PersonDto personDto) throws Exception {
+		return personRepository.existsByDocument(personDto.getDocument());
+	}
+
+	@Override
+	public void createPerson(PersonDto personDto) throws Exception {
+		Person person = Helper.parse(personDto);
+		personRepository.save(person);
+	}
+
+	@Override
+	public void deletePerson(PersonDto personDto) throws Exception {
+		Person person = Helper.parse(personDto);
+		personRepository.delete(person);
+        }
+
+	@Override
+	public PersonDto findByDocument(PersonDto personDto) throws Exception {
+		Person person = personRepository.findByDocument(personDto.getDocument());
+                return Helper.parse(person);
+	}
+
+}
---
+++ ejemploConSpringBoot/src/main/java/app/model/User.java
@@ -0,0 +1,73 @@
+package app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @JoinColumn(name="personid")
+    @OneToOne
+    private Person personId;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "username")
+    private String userName;
+    @Column(name = "role")
+    private String role;
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Person getPersonid() {
+        return personId;
+    }
+
+    public void setPersonid(Person personid) {
+        this.personId = personid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
